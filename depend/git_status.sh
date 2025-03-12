065e10af57a2c526c77be7dda931b98e565f5d70
chenq <291327111@qq.com>
2025-03-12 09:06:05 +0800

    修改git_status.sh

--- depend/git_status.sh
+++ depend/git_status.sh
@@ -16,12 +16,12 @@ cd "$project_path" || { echo "fatal: Failed to enter local repository directory.
 # 获取本地最新一次提交的哈希值
 local_latest_commit=$(git log -1 --pretty=format:'%H')
 
-echo "local_latest_commit: $local_latest_commit"
+echo "[local] commit: $local_latest_commit"
 
 # 判断XML中的revision是否是 hash， 然后与本地的哈希作比较
 if [[ "$remote_revision" =~ ^[0-9a-f]{40}$ ]]; then
     if [ ! "$remote_revision" = "$local_latest_commit" ]; then
-        echo "revision: $remote_revision"
+        echo "[XML] commit: $remote_revision"
     fi
 fi
 
@@ -33,19 +33,19 @@ for remote_name in $remotes; do
     # 获取远程最新的提交 hash， 然后与本地的哈希作比较
     remote_latest_commit=$(git ls-remote "$remote_name" "$remote_branch" | awk '{print $1}')
     if [ ! "$remote_latest_commit" = "$local_latest_commit" ]; then
-        echo "remote_latest_commit: $remote_latest_commit $remote_name $remote_branch"
+        echo "[remote] commit: $remote_latest_commit $remote_name $remote_branch"
     fi
 done
 
 # 获取本地分支的个数（排除空行）
 branch_count=$(git branch --format='%(refname:short)' | wc -l)
 if (( branch_count > 1 )); then
-    echo "branch_count: $branch_count"
+    echo "[branch]: it has $branch_count branch"
 fi
 
 # 查看是否有未提交的修改
 status_info=$(git status)
 search_str="nothing to commit, working tree clean"
 if [[ ! "$status_info" == *"$search_str"* ]]; then
-  echo "Changes not staged for commit"
+  echo "[status] changes not staged for commit"
 fi
